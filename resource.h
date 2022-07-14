ceeb5b23e91c5f4435c7439c3beda8775a1ce6ea
Sarnax <SarnaxOfficial@protonmail.com>
2022-07-15 02:26:50 +0700

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,16 +1,15 @@
 //{{NO_DEPENDENCIES}}
 // fichier Include Microsoft Visual C++.
-// Utilisé par Resource.rc
+// UtilisÃ© par Resource.rc
 //
 #define IDI_ICON1                       101
 
 // Next default values for new objects
-// 
+// Offset Not Last version Please checking again want update..
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
+#define _APS_NEXT_SYMED_VALUE           101,1005,2052
 #endif
