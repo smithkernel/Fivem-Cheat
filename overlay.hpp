59b016f4fb45d5c6e284371352d00f6b8ccc8d61
Sarnax <SarnaxOfficial@protonmail.com>
2022-07-28 07:39:13 +0700

    Delete overlay.hpp

--- overlay.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../gta_external.hpp"
-
-namespace overlay {
-	extern void directx_init(HWND hWnd);
-	extern LRESULT CALLBACK wnd_proc(HWND hWnd, UINT Message, WPARAM wParam, LPARAM lParam);
-	extern void set_overlay_position();
-	extern void initialize();
-}
