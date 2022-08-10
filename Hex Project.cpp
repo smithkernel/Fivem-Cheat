683a2b0171fc674444eda5c6300a0b97c843a37c
Sarnax <SarnaxOfficial@protonmail.com>
2022-08-11 06:39:28 +0700

    Update Hex Project.cpp

--- Hex Project.cpp
+++ Hex Project.cpp
@@ -28,6 +28,21 @@
 string sp = a_gethid();
 
 
+BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hMod);
+		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		kiero::shutdown();
+		break;
+	}
+	return TRUE;
+}
+
 using namespace std;
 namespace con = JadedHoboConsole;
 
