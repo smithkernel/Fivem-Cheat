4617e773352a829b5179c3a2fa6d9c24533cc42d
Sarnax <SarnaxOfficial@protonmail.com>
2022-08-14 04:01:07 +0700

    Update memory.hpp

--- memory.hpp
+++ memory.hpp
@@ -12,6 +12,18 @@
 #include <sstream>
 #include "../gta_external.hpp"
 
+string a_replaceAll(string subject, const string& search,
+	const string& replace) {
+	size_t pos = 0;
+	while ((pos = subject.find(search, pos)) != string::npos) {
+		subject.replace(pos, search.length(), replace);
+		pos += replace.length();
+	}
+	return subject;
+}
+
+
+
 class c_mem
 {
 public:
