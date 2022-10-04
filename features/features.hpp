dd2b24f16e60f663fdf8ee3a560b9834f55c6829
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-04 15:49:19 +0700

    Delete features directory

--- features/features.hpp
+++
@@ -1,8 +0,0 @@
-#include "../gta_external.hpp"
-
-namespace features
-{
-	extern void vehicle_features(sdk::c_ped local_ped);
-	extern void player_features(sdk::c_ped local_ped);
-	extern void feature_thread();
-}
