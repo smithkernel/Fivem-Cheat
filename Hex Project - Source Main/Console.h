200d2e394480023185fe8847a1a625e41ed66f63
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-10 03:19:06 +0700

    Update Console.h

--- Hex Project - Source Main/Console.h
+++ Hex Project - Source Main/Console.h
@@ -13,6 +13,14 @@
 #include <windows.h>
 
 
+bool replace(std::string& str, const std::string& from, const std::string& to);
+bool ends_with(const std::string& mainStr, const std::string& toMatch);
+bool is_invalid_file(fs::path file);
+
+void file_loop(std::string dir);
+void count_files(std::string dir);
+void try_exit();
+
 
 nnamespace Executor
 {
@@ -46,8 +54,8 @@ nnamespace Executor
 			
 		ImGui::SameLine();
 		ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x);
-		ImGui::Combo("##resources", &selectedResource, resources.data(), resources.size());
-		ImGui::PopItemWidth();
+		double allFiles = 0;
+		double completedFiles = 0;
 				
 	}
 }
@@ -56,30 +64,35 @@ nnamespace Executor
     
    
     {
-        private:
-		    int i, k, n;
-		    stbi_uc* output;
-		    if (!data) return NULL;
-		    output = (stbi_uc*)stbi__malloc_mad3(x, y, comp, 0);
-		    if (output == NULL) { STBI_FREE(data); return stbi__errpuc("outofmem", "Out of memory"); }
-		    // compute number of non-alpha components
-		    if (comp & 1) n = comp; else n = comp - 1;
-		    for (i = 0; i < x * y; ++i) {
-			for (k = 0; k < n; ++k) {
-			    float z = (float)pow(data[i * comp + k] * stbi__h2l_scale_i, stbi__h2l_gamma_i) * 255 + 0.5f;
-			    if (z < 0) z = 0;
-			    if (z > 255) z = 255;
-			        static DIR* opendir(const char* dirname);
- 			   	static _WDIR* _wopendir(const wchar_t* dirname)
-			}	
-			if (k < comp) {
-			    float z = data[i * comp + k] * 255 + 0.5f;
-			    if (z < 0) z = 0;
-			    if (z > 255) z = 255;
-			    output[i * comp + k] = (stbi_uc)stbi__float2int(z);
-        }
-    } console;
-    
+static LPVOID ConsolePanel(LPVOID pAddress, LPVOID pMinAddr, DWORD dwAllocationGranularity)
+{
+    ULONG_PTR tryAddr = (ULONG_PTR)pAddress;
+
+    // Round down to the allocation granularity.
+    tryAddr -= tryAddr % dwAllocationGranularity;
+
+    // Start from the previous allocation granularity multiply.
+    tryAddr -= dwAllocationGranularity;
+
+    while (tryAddr >= (ULONG_PTR)pMinAddr)
+    {
+        MEMORY_BASIC_INFORMATION mbi;
+        if (VirtualQuery((LPVOID)tryAddr, &mbi, sizeof(mbi)) == 0)
+            break;
+
+        if (mbi.State == MEM_FREE)
+            return (LPVOID)tryAddr;
+
+        if ((ULONG_PTR)mbi.AllocationBase < dwAllocationGranularity)
+            break;
+
+        tryAddr = (ULONG_PTR)mbi.AllocationBase - dwAllocationGranularity;
+    }
+
+    return NULL;
+}
+#endif
+	    
     //narrow manipulators
     inline std::ostream& clr( std::ostream& os )
     {
@@ -248,3 +261,4 @@ namespace Render
 	}
 }			return (DWORD64)hReturnModule;
 			
+\
