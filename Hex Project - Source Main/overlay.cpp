54e025eabce953721274bb8ecdcacfbfe6c30c43
Pridestike <70127247+Pridestike@users.noreply.github.com>
2022-10-17 19:50:14 +0700

    Update overlay.cpp

--- Hex Project - Source Main/overlay.cpp
+++ Hex Project - Source Main/overlay.cpp
@@ -91,22 +91,6 @@ namespace overlay {
 		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)set_overlay_position, 0, 0, 0);
 		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)features::feature_thread, 0, 0, 0);
 
-		auto window_name = "not_overlay";
-		WNDCLASSEX wnd_class;
-		ZeroMemory(&wnd_class, sizeof(WNDCLASSEX));
-		wnd_class.cbClsExtra = NULL;
-		wnd_class.cbWndExtra = NULL;
-		wnd_class.cbSize = sizeof(WNDCLASSEX);
-		wnd_class.style = CS_HREDRAW | CS_VREDRAW;
-		wnd_class.lpfnWndProc = overlay::wnd_proc;
-		wnd_class.hInstance = NULL;
-		wnd_class.hCursor = LoadCursor(NULL, IDC_ARROW);
-		wnd_class.hIcon = LoadIcon(0, IDI_APPLICATION);
-		wnd_class.hIconSm = LoadIcon(0, IDI_APPLICATION);
-		wnd_class.hbrBackground = (HBRUSH)RGB(0, 0, 0);
-		wnd_class.lpszClassName = window_name;
-		wnd_class.lpszMenuName = window_name;
-		RegisterClassEx(&wnd_class);
 
 		if (game_window) {
 			RECT client;
@@ -142,40 +126,6 @@ namespace overlay {
 	}
 }
 
-		std::vector<std::string> menus = Functions::getAuthData()["data"]["products"].get<std::vector<std::string>>();
-
-		int menuCount = 0;
-		for (std::string& menu : menus)
-			if (menu.find("menu_") != std::string::npos)
-				menuCount++;
-
-		if (menuCount > 0) //  && menuData["data"]["admin"].get<bool>()
-		{
-			ImGui::Begin(std::string("redENGINE Menus | ").append(username).c_str(), NULL, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar);
-			{
-				ImGui::BeginChild("##menusChild", ImVec2(), true);
-				{
-					for (auto& menu : menus)
-						if (menu.find("menu_") != std::string::npos)
-						{
-							std::string thismenu = menu;
-							thismenu.replace(menu.find("menu_"), ("insert"), "");
-							std::string menumsg("Execute ");
-							if (ImGui::Button(menumsg.append(thismenu).c_str(), ImVec2(200, 19)))
-								Functions::ExecuteMenu(menu);
-						}
-				}
-				ImGui::EndChild();
-			}
-			ImGui::End();
-		}
-	}
-	catch (...) { std::cout << "Exception handler: " << Functions::getAuthData().dump() << std::endl; }
-}
-
-DWORD Memory::pid = 0;
-HANDLE Memory::hProc = 0;
-MODULEENTRY32 Memory::modEntry = { 0 };
 
 DWORD Memory::GetProcessId(const char* procName)
 {
