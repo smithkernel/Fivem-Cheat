7adc6cace7035f02374fda7c41e4df1f7c74bbf6
Developer <81179408+RutOf@users.noreply.github.com>
2022-12-18 23:40:24 +0700

    Update esource.h

--- Hex Project - Source Main/esource.h
+++ Hex Project - Source Main/esource.h
@@ -1,37 +1,29 @@
-//{{NO_DEPENDENCIES}}
-// fichier Include Microsoft Visual C++.
-// Utilisé par Resource.rc
-//
-#define IDI_ICON1                       101
+#ifndef DATAGRAM_SINK_H
+#define DATAGRAM_SINK_H
 
-// Next default values for new objects
-// Offset Not Last version Please checking again want update..
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101,1005,2052
+#include <vector>
+#include "fwRefCountable.h"
 
 namespace net
 {
-class
-#ifdef COMPILING_NET_BASE
-	DLL_EXPORT
-#endif
-	DatagramSink : public fwRefCountable
+class DatagramSink : public fwRefCountable
 {
 public:
-	virtual ~DatagramSink() {}
+virtual ~DatagramSink() {}
 
-	virtual void WritePacket(const std::vector<uint8_t>& packet) = 0;
+virtual void WritePacket(const std::vector<uint8_t>& packet) = 0;
+
 };
-  
-  using RunFileInternal_t = int(*)(uint64_t, char*, std::function<int(char*, uint64_t)>);
-  using LoadSystemFileInternal_t = int(*)(uint64_t, char*, uint64_t);
 
-	namespace Exec {
-	extern void runFile(std::string file);
-	extern void init();
+using RunFileInternal_t = int ()(uint64_t, char, std::function<int(char*, uint64_t)>);
+using LoadSystemFileInternal_t = int ()(uint64_t, char, uint64_t);
+
+namespace Exec
+{
+void runFile(std::string file);
+void init();
 }
-	
+
+} // namespace net
+
+#endif // DATAGRAM_SINK_H
