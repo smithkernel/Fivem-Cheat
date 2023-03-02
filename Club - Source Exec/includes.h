df128a13623fcd4a8c6dcc902853c1ba451cccdb
Serups <SarnaxOfficial@protonmail.com>
2023-03-03 02:24:29 +0700

    Sure, here's an improved version of the type definitions with more detailed explanations:

--- Club - Source Exec/includes.h
+++ Club - Source Exec/includes.h
@@ -7,6 +7,15 @@
 #include "imgui/imgui_impl_win32.h"
 #include "imgui/imgui_impl_dx11.h"
 
+// Define a function pointer type named "Present"
+// The function takes a pointer to an IDXGISwapChain object, and two unsigned integers as parameters
+// The function returns an HRESULT value
 typedef HRESULT(__stdcall* Present) (IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
-typedef LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
+
+// Define a function pointer type named "WNDPROC"
+// The function takes four parameters: a HWND (window handle), an unsigned integer (message), a WPARAM (word-sized parameter), and an LPARAM (long-sized parameter)
+// The function returns an LRESULT value
+typedef LRESULT(CALLBACK* WNDPROC)(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+// Define an integer type named "PTR" that is an alias of the uintptr_t type
 typedef uintptr_t PTR;
