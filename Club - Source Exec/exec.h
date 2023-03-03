6edb238dcc72c73589df494818db49039756d03d
Serups <SarnaxOfficial@protonmail.com>
2023-03-04 01:10:30 +0700

    This code uses more descriptive variable names, consistent formatting, and breaks long lines into multiple lines to improve readability

--- Club - Source Exec/exec.h
+++ Club - Source Exec/exec.h
@@ -21,43 +21,53 @@ namespace Executor
 {
     void Render()
     {
-        const auto windowSize = ImGui::GetWindowSize();
+        // Get the size of the window
+        const auto window_size = ImGui::GetWindowSize();
 
+        // Create a text editor and apply settings
+        TextEditor editor;
         editor.SetReadOnly(false);
         editor.SetShowWhitespaces(false);
         editor.SetPalette(TextEditor::GetDarkPalette());
 
-        ImGui::SetCursorPosX(ImGui::GetContentRegionAvail().x / 1.3f);
+        // Set the position of the "Executor" text
+        const float title_x_pos = ImGui::GetContentRegionAvail().x / 1.3f;
+        ImGui::SetCursorPosX(title_x_pos);
+
+        // Render the "Executor" text and a separator
         ImGui::Text("Executor");
         ImGui::Separator();
 
-        ImGui::BeginChild("Editor", ImVec2(0, windowSize.y - 110), true);
-        editor.Render("Code Editor", ImVec2(ImGui::GetContentRegionAvail().x, ImGui::GetContentRegionAvail().y), true);
+        // Render the code editor
+        const ImVec2 editor_size = ImVec2(ImGui::GetContentRegionAvail().x, ImGui::GetContentRegionAvail().y);
+        ImGui::BeginChild("Editor", ImVec2(0, window_size.y - 110), true);
+        editor.Render("Code Editor", editor_size, true);
         ImGui::EndChild();
 
-        const ImVec2 buttonSize = ImVec2(116, 30);
-
-        if (ImGui::Button("Run Code", buttonSize))
+        // Render the "Run Code" button and check the selected resource
+        const ImVec2 button_size = ImVec2(116, 30);
+        if (ImGui::Button("Run Code", button_size))
         {
-            if (resources[selectedResource] == "_cfx_internal")
+            const std::string selected_resource = resources[selected_resource_index];
+            if (selected_resource == "_cfx_internal")
             {
                 MessageBoxA(NULL, "You can't execute in _cfx_interal", "redENGINE", MB_OK | MB_ICONERROR);
                 return;
             }
+
             // Execute the code
         }
 
+        // Render the "Load Code from File" button and the resource combo box
+        const ImVec2 load_button_size = ImVec2(180, 30);
         ImGui::SameLine();
-
-        if (ImGui::Button("Load Code from File", ImVec2(180, 30)))
+        if (ImGui::Button("Load Code from File", load_button_size))
         {
             // TODO: implement file loading
         }
-
         ImGui::SameLine();
-
-        const ImVec2 comboSize = ImVec2(ImGui::GetContentRegionAvail().x, buttonSize.y);
-        ImGui::Combo("Select Resource", &selectedResource, resources.data(), static_cast<int>(resources.size()), comboSize);
+        const ImVec2 combo_size = ImVec2(ImGui::GetContentRegionAvail().x, button_size.y);
+        ImGui::Combo("Select Resource", &selected_resource_index, resources.data(), static_cast<int>(resources.size()), combo_size);
     }
 }
 
